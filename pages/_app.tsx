40e4fbb65e397a0adf77e18f06f00540113f09d4
huntedman <toomashaide@gmail.com>
2021-11-27 14:14:40 +0200

    Broken commit.
    Initialized a new nextjs project and tried using i18n, but it doesn't work for some reason.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import TypesafeI18n from '../src/i18n/i18n-react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <TypesafeI18n initialLocale="en">
+      <Component {...pageProps} />
+    </TypesafeI18n>
+  );
 }
 
-export default MyApp
+export default MyApp;
